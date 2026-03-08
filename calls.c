928b3eae69ac4a2a0dddd6fb98cb6d06ff0f24c7
caesesia <marsel.msc@proton.me>
2026-03-08 01:12:18 +0100

    feat: add device listing and handle creation (hardcoded device)

--- calls.c
+++ calls.c
@@ -5,23 +5,29 @@ int main() {
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_if_t *alldevs;
 	int dev_list = pcap_findalldevs(&alldevs, errbuf);
-	pcap_t *handle;
-	int ret_handle = pcap_create(alldevs->name, errbuf);
+	pcap_t *handle = pcap_create("wlp61s0", errbuf);
+	
+	// Device listing
 	if (dev_list != 0) {
 		puts("Error finding devices to listen on.");
 		printf("Error : %s\n", errbuf);
 		return 1;
 	} else {
+		int n = 1;
 		puts("Success finding devices to listen on.");
-		printf("List of devices : %s\n", d->name);
-		return 0;
+		for (pcap_if_t *i = alldevs; i != NULL; i = i->next) {
+			printf("	* Device %d : %s\n", n, i->name);
+			n++;
+		}
 	}
-	if (ret_handle != 0) {
+	
+	// Handle creation
+	if (handle == NULL) {
 		puts("Error creating the handle.");
 		printf("Error : %s\n", errbuf);
 		return 1;
 	} else {
-		puts("Success creating the handle.");
+		puts("Success creating the handle for device [wlp61s0].");
 		return 0;
 	}
 }
