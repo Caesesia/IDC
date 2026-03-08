76ae8328162e14992b3bd311016fe1b3db0d9314
caesesia <marsel.msc@proton.me>
2026-03-08 01:59:09 +0100

    feat: add packet capture and print length

--- calls.c
+++ calls.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <pcap/pcap.h>
 
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+	printf("Packet captured! Length: %d\n", header->len);
+}
+
 int main() {
 	char errbuf[PCAP_ERRBUF_SIZE];
+	const char *device = "wlp61s0";
+	
 	pcap_if_t *alldevs;
 	int dev_list = pcap_findalldevs(&alldevs, errbuf);
-	pcap_t *handle = pcap_create("wlp61s0", errbuf);
 	
+	pcap_t *handle = pcap_create(device, errbuf);
+
+	// Add timeout to capture after 1s even if buffer isn't filled
+	pcap_set_timeout(handle, 1000);
+	
+	int activate = pcap_activate(handle);
+
 	// Device listing
 	if (dev_list != 0) {
 		puts("Error finding devices to listen on.");
@@ -14,7 +26,7 @@ int main() {
 		return 1;
 	} else {
 		int n = 1;
-		puts("Success finding devices to listen on.");
+		puts("\nSuccess finding devices to listen on.");
 		for (pcap_if_t *i = alldevs; i != NULL; i = i->next) {
 			printf("	* Device %d : %s\n", n, i->name);
 			n++;
@@ -23,11 +35,25 @@ int main() {
 	
 	// Handle creation
 	if (handle == NULL) {
-		puts("Error creating the handle.");
+		puts("\nError creating the handle.");
 		printf("Error : %s\n", errbuf);
 		return 1;
 	} else {
-		puts("Success creating the handle for device [wlp61s0].");
-		return 0;
+		printf("\nSuccess creating the handle for device %s.\n", device);
 	}
+
+	if (activate == 0) {
+		printf("\nSuccess activating the handle %s.\n\n", device);
+	} else {
+		printf("\nError activating the handle %s.\n", device);
+		printf("Error : %s\n", pcap_statustostr(activate));
+		return 1;
+	}
+
+	printf("Starting capture...\n\n");
+	pcap_loop(handle, 5, got_packet, NULL);
+
+	pcap_close(handle);
+	pcap_freealldevs(alldevs);
+	return 0;
 }
